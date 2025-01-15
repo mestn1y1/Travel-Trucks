90c9a6fb17217be88cd75f1395c15cbbec66b037
Serhii Tyron <mestn1y@hotmail.com>
2025-01-15 23:42:32 +0200

    add condition render

--- src/pages/CamperDetailsPage/CamperDetails.jsx
+++ src/pages/CamperDetailsPage/CamperDetails.jsx
@@ -1,3 +1,79 @@
+// import { Outlet, NavLink } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useParams } from "react-router-dom";
+
+// import { fetchCamperById } from "../../redux/campers/operations";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   selectCurrentCamper,
+//   selectIsLoading,
+// } from "../../redux/campers/selectors";
+// import { Icon } from "../../components/Icon/Icon";
+// import clsx from "clsx";
+// import css from "./CamperDetails.module.css";
+
+// const getClassName = (props) => {
+//   return clsx(css.link, props.isActive && css.active);
+// };
+
+// export default function CamperDetailsPage() {
+//   const { id } = useParams();
+//   const dispatch = useDispatch();
+//   const loading = useSelector(selectIsLoading);
+//   useEffect(() => {
+//     dispatch(fetchCamperById(id));
+//   }, [id, dispatch]);
+//   const currentCamper = useSelector(selectCurrentCamper);
+//   console.log(currentCamper);
+//   return (
+//     <div className={css.wrapper}>
+//       <h2 className={css.currentCamperText}>{currentCamper?.name}</h2>
+//       <div className={css.ratingInfo}>
+//         <p className={css.camperRating}>
+//           <Icon iconName="star-pressed" className={css.iconsStar} />
+//           {currentCamper.rating}
+//           <span>({currentCamper.reviews.length} Reviews)</span>
+//         </p>
+//         <p className={css.camperLocation}>
+//           <Icon iconName="map" className={css.iconsStar} />
+//           {currentCamper.location}
+//         </p>
+//       </div>
+//       <p className={css.currentCamperText}>€{currentCamper.price.toFixed(2)}</p>
+
+//       <ul className={css.currentCamperGallery}>
+//         {currentCamper.gallery.map((item, index) => (
+//           <li key={index} className={css.galleryItem}>
+//             <img
+//               src={item.original}
+//               alt={`Gallery item ${index + 1}`}
+//               className={css.galleryImage}
+//             />
+//           </li>
+//         ))}
+//       </ul>
+
+//       <p className={css.currentCamperDescription}>
+//         {currentCamper.description}
+//       </p>
+
+//       <ul className={css.currentCamperDetails}>
+//         <li>
+//           <NavLink to="features" className={getClassName}>
+//             Features
+//           </NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="reviews" className={getClassName}>
+//             Reviews
+//           </NavLink>
+//         </li>
+//       </ul>
+//       <hr className={css.horizontLine} />
+//       <Outlet />
+//     </div>
+//   );
+// }
 import { Outlet, NavLink } from "react-router-dom";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
@@ -20,55 +96,64 @@ export default function CamperDetailsPage() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const loading = useSelector(selectIsLoading);
+  const currentCamper = useSelector(selectCurrentCamper);
   useEffect(() => {
     dispatch(fetchCamperById(id));
   }, [id, dispatch]);
-  const currentCamper = useSelector(selectCurrentCamper);
-  console.log(currentCamper);
+
+  // Conditionally render content based on `currentCamper`
   return (
     <div className={css.wrapper}>
-      <h2 className={css.currentCamperText}>{currentCamper?.name}</h2>
-      <div className={css.ratingInfo}>
-        <p className={css.camperRating}>
-          <Icon iconName="star-pressed" className={css.iconsStar} />
-          {currentCamper.rating}
-          <span>({currentCamper.reviews.length} Reviews)</span>
-        </p>
-        <p className={css.camperLocation}>
-          <Icon iconName="map" className={css.iconsStar} />
-          {currentCamper.location}
-        </p>
-      </div>
-      <p className={css.currentCamperText}>€{currentCamper.price.toFixed(2)}</p>
+      {currentCamper ? (
+        <>
+          <h2 className={css.currentCamperText}>{currentCamper.name}</h2>
+          <div className={css.ratingInfo}>
+            <p className={css.camperRating}>
+              <Icon iconName="star-pressed" className={css.iconsStar} />
+              {currentCamper.rating}
+              <span>({currentCamper.reviews.length} Reviews)</span>
+            </p>
+            <p className={css.camperLocation}>
+              <Icon iconName="map" className={css.iconsStar} />
+              {currentCamper.location}
+            </p>
+          </div>
+          <p className={css.currentCamperText}>
+            €{currentCamper.price.toFixed(2)}
+          </p>
 
-      <ul className={css.currentCamperGallery}>
-        {currentCamper.gallery.map((item, index) => (
-          <li key={index} className={css.galleryItem}>
-            <img
-              src={item.original}
-              alt={`Gallery item ${index + 1}`}
-              className={css.galleryImage}
-            />
-          </li>
-        ))}
-      </ul>
+          <ul className={css.currentCamperGallery}>
+            {currentCamper.gallery.map((item, index) => (
+              <li key={index} className={css.galleryItem}>
+                <img
+                  src={item.original}
+                  alt={`Gallery item ${index + 1}`}
+                  className={css.galleryImage}
+                />
+              </li>
+            ))}
+          </ul>
 
-      <p className={css.currentCamperDescription}>
-        {currentCamper.description}
-      </p>
+          <p className={css.currentCamperDescription}>
+            {currentCamper.description}
+          </p>
 
-      <ul className={css.currentCamperDetails}>
-        <li>
-          <NavLink to="features" className={getClassName}>
-            Features
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="reviews" className={getClassName}>
-            Reviews
-          </NavLink>
-        </li>
-      </ul>
+          <ul className={css.currentCamperDetails}>
+            <li>
+              <NavLink to="features" className={getClassName}>
+                Features
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="reviews" className={getClassName}>
+                Reviews
+              </NavLink>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <p className={css.errorText}>Camper details could not be loaded.</p>
+      )}
       <hr className={css.horizontLine} />
       <Outlet />
     </div>
