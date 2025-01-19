f8c1ec54527917d5fd85f59a90e1b9a5a9f5d597
Serhii Tyron <mestn1y@hotmail.com>
2025-01-19 23:07:21 +0200

    add notfound page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import css from "./NotFoundPage.module.css";
+
 export default function NotFoundPage() {
   return (
-    <>
-      <h2>Not found 404</h2>
-    </>
+    <div className={css.wrapper}>
+      <h2 className={css.title}>Not found 404</h2>
+      <Link to="/">
+        <Button text="Back Home" />
+      </Link>
+    </div>
   );
 }
